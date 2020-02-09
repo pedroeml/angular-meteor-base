305919cc2fc141e84e72c66e8896171ee2fc060c
pedroeml <pedroemlemos@gmail.com>
2020-02-09 15:05:54 -0300

    Including routing module to AppModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,49 +2,21 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { TodoAddComponent } from './todo-add/todo-add.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'todoList',
-        component: TodoListComponent
-      },
-      {
-        path: 'todoAdd',
-        component: TodoAddComponent
-      },
-      // Home Page
-      {
-        path: '',
-        redirectTo: '/todoList',
-        pathMatch: 'full'
-      },
-      // 404 Page
-      {
-        path: '**',
-        component: PageNotFoundComponent
-      }
-    ])
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
-    TodoAddComponent,
-    TodoListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   bootstrap: [
-    AppComponent
+    AppComponent,
   ]
 })
 export class AppModule { }
