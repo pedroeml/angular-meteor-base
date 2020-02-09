efb85487b0af413a96ac6d9c0c91b93a45ed8bfd
pedroeml <pedroemlemos@gmail.com>
2020-02-09 17:46:26 -0300

    FormGroupFactory bug fix

--- client/imports/app/films/factory/form-group.factory.ts
+++ client/imports/app/films/factory/form-group.factory.ts
@@ -17,12 +17,22 @@ export class FormGroupFactory {
     const title: string = !isNullOrUndefined(film) && !isNullOrUndefined(film.title) ? film.title : '';
     const director: string = !isNullOrUndefined(film) && !isNullOrUndefined(film.director) ? film.director : '';
     const producer: string = !isNullOrUndefined(film) && !isNullOrUndefined(film.producer) ? film.producer : '';
-    const releaseDate: string = this.datePipe.transform(!isNullOrUndefined(film) && !isNullOrUndefined(film.releaseDate) ? new Date(film.releaseDate) : new Date(), 'yyyy-MM-dd');
+    const releaseDate: string = this.datePipe.transform(this.createDate(film), 'yyyy-MM-dd');
+
     return this.formBuilder.group({
       titleCtrl: [title],
       directorCtrl: [director],
       producerCtrl: [producer],
       releaseDateCtrl: [releaseDate],
     });
+  }
+
+  private createDate(film: Film): Date {
+    if (isNullOrUndefined(film) || isNullOrUndefined(film.releaseDate)) {
+      return new Date();
+    }
+
+    const splitted: string[] = film.releaseDate.split('-');
+    return new Date(+splitted[0], +splitted[1], +splitted[2]);
   }
 }
